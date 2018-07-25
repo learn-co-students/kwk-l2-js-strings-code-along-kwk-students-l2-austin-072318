15cc2bbc500cbb81a90470de165073394911ec47
AngelaMMartinez <angela.murillo.martinezz@gmail.com>
2018-07-25 15:32:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // Write your code in this file!
-
+let currentUser="Angela Murillo"
+let welcomeMessage="Welcome to Klossybook, "
+welcomeMessage= welcomeMessage+currentUser
+welcomeMessage=welcomeMessage + "!"
+let excitedWelcomeMessage="WELCOME TO KLOSSYBOOK, "
+excitedWelcomeMessage=excitedWelcomeMessage+currentUser.toUpperCase()
+excitedWelcomeMessage=excitedWelcomeMessage+"!"
+let shortGreeting= "Welcome, " 
+shortGreeting = shortGreeting+ currentUser.charAt(0) 
+shortGreeting=shortGreeting + "!"
