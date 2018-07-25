5ef7753b84f6b0b9176151c593abc9c04b354409
Katelyn Rodrigues <katelyn.rodrigues1@gmail.com>
2018-07-25 15:26:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // Write your code in this file!
+var currentUser = "Katelyn Rodrigues"
 
+var welcomeMessage = "Welcome to Klossybook, " + currentUser + "!"
+
+var excitedWelcomeMessage = "WELCOME TO KLOSSYBOOK, "+ currentUser.toUpperCase() + "!"
+
+var shortGreeting="Welcome, " + currentUser.slice(0,2) + "!"
