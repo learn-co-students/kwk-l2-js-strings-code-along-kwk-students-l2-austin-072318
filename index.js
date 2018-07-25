54824f5bd2416362fecad1911f8c4b8ed52bb738
NayeliGarcia <nayeli.garcia2222@gmail.com>
2018-07-25 15:28:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 // Write your code in this file!
-
+let currentUser = "Nayeli Garcia"
+let welcomeMessage ="Welcome to Klossybook, "
+welcomeMessage = welcomeMessage + currentUser + "!"
+let excitedWelcomeMessage= welcomeMessage.toUpperCase() 
+let shortGreeting = "Welcome, " + currentUser. slice (0,1) + "!"
